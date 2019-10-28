54bc4d878101b864da922ab887e678cb8d923b88
MrCrayfish <mrcrayfish@hotmail.com>
2019-10-29 00:06:20 +1030

    Created basic template for all mods

---
+++ src/main/java/com/mrcrayfish/modid/Reference.java
@@ -0,0 +1,9 @@
+package com.mrcrayfish.modid;
+
+/**
+ * Author: MrCrayfish
+ */
+public class Reference
+{
+    public static final String MOD_ID = "modid";
+}
---
+++ src/main/java/com/mrcrayfish/modid/TemplateMod.java
@@ -0,0 +1,11 @@
+package com.mrcrayfish.modid;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mod(Reference.MOD_ID)
+public class TemplateMod
+{
+}
