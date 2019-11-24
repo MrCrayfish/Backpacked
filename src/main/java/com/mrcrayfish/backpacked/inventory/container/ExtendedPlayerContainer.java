debad25e91f350f30f37c64d2cf6f7c09583f2e2
MrCrayfish <mrcrayfish@hotmail.com>
2019-11-24 13:48:39 +1030

    Backpack can now quick move into backpack slot

--- src/main/java/com/mrcrayfish/backpacked/inventory/container/ExtendedPlayerContainer.java
+++ src/main/java/com/mrcrayfish/backpacked/inventory/container/ExtendedPlayerContainer.java
@@ -1,8 +1,10 @@
 package com.mrcrayfish.backpacked.inventory.container;
 
 import com.mrcrayfish.backpacked.core.ModItems;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.inventory.container.PlayerContainer;
 import net.minecraft.inventory.container.Slot;
 import net.minecraft.item.ItemStack;
@@ -34,5 +36,106 @@ public class ExtendedPlayerContainer extends PlayerContainer
                 return stack.getItem() == ModItems.BACKPACK;
             }
         });
+    }
+
+    @Override
+    public ItemStack transferStackInSlot(PlayerEntity playerIn, int index)
+    {
+        ItemStack copy = ItemStack.EMPTY;
+        Slot slot = this.inventorySlots.get(index);
+        if(slot != null && slot.getHasStack())
+        {
+            ItemStack slotStack = slot.getStack();
+            copy = slotStack.copy();
+            EquipmentSlotType equipmentslottype = MobEntity.getSlotForItemStack(copy);
+            if(index != 46 && copy.getItem() == ModItems.BACKPACK)
+            {
+                if(!this.inventorySlots.get(46).getHasStack())
+                {
+                    if(!this.mergeItemStack(slotStack, 46, 47, false))
+                    {
+                        return ItemStack.EMPTY;
+                    }
+                }
+            }
+            else if(index == 0)
+            {
+                if(!this.mergeItemStack(slotStack, 9, 45, true))
+                {
+                    return ItemStack.EMPTY;
+                }
+
+                slot.onSlotChange(slotStack, copy);
+            }
+            else if(index >= 1 && index < 5)
+            {
+                if(!this.mergeItemStack(slotStack, 9, 45, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if(index >= 5 && index < 9)
+            {
+                if(!this.mergeItemStack(slotStack, 9, 45, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if(equipmentslottype.getSlotType() == EquipmentSlotType.Group.ARMOR && !this.inventorySlots.get(8 - equipmentslottype.getIndex()).getHasStack())
+            {
+                int i = 8 - equipmentslottype.getIndex();
+                if(!this.mergeItemStack(slotStack, i, i + 1, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if(equipmentslottype == EquipmentSlotType.OFFHAND && !this.inventorySlots.get(45).getHasStack())
+            {
+                if(!this.mergeItemStack(slotStack, 45, 46, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if(index >= 9 && index < 36)
+            {
+                if(!this.mergeItemStack(slotStack, 36, 45, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if(index >= 36 && index < 45)
+            {
+                if(!this.mergeItemStack(slotStack, 9, 36, false))
+                {
+                    return ItemStack.EMPTY;
+                }
+            }
+            else if(!this.mergeItemStack(slotStack, 9, 45, false))
+            {
+                return ItemStack.EMPTY;
+            }
+
+            if(slotStack.isEmpty())
+            {
+                slot.putStack(ItemStack.EMPTY);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if(slotStack.getCount() == copy.getCount())
+            {
+                return ItemStack.EMPTY;
+            }
+
+            ItemStack itemstack2 = slot.onTake(playerIn, slotStack);
+            if(index == 0)
+            {
+                playerIn.dropItem(itemstack2, false);
+            }
+        }
+
+        return copy;
     }
 }
