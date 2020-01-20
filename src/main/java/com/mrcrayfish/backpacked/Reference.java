926153d1ab24005b99ade79514c914b3460e7c4a
MrCrayfish <mrcrayfish@hotmail.com>
2020-01-20 12:48:47 +1030

    Updated version

--- src/main/java/com/mrcrayfish/backpacked/Reference.java
+++ src/main/java/com/mrcrayfish/backpacked/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 {
     public static final String MOD_ID = "backpacked";
     public static final String MOD_NAME = "Backpacked";
-    public static final String MOD_VERSION = "1.3.0";
+    public static final String MOD_VERSION = "1.4.0";
     public static final String ACCEPTED_MC_VERSIONS = "[1.12.2]";
     public static final String CLIENT_PROXY = "com.mrcrayfish.backpacked.proxy.ClientProxy";
     public static final String COMMON_PROXY = "com.mrcrayfish.backpacked.proxy.CommonProxy";
