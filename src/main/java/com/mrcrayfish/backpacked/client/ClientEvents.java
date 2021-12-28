7e7b594c8ead0e17c00125f1e91ac533847db531
MrCrayfish <mrcrayfish@hotmail.com>
2021-12-29 01:35:36 +1030

    📦 Improved pickpocket debug rendering

--- src/main/java/com/mrcrayfish/backpacked/client/ClientEvents.java
+++ src/main/java/com/mrcrayfish/backpacked/client/ClientEvents.java
@@ -18,6 +18,7 @@ import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraftforge.client.event.InputEvent;
@@ -141,14 +142,15 @@ public class ClientEvents
             IVertexBuilder builder = source.getBuffer(RenderType.lines());
             WorldRenderer.renderLineBox(stack, builder, PickpocketUtil.getBackpackBox(player, event.getPartialTicks()), boxRed, boxGreen, boxBlue, 1.0F);
 
+            float bodyRotation = MathHelper.lerp(event.getPartialTicks(), player.yBodyRotO, player.yBodyRot);
             boolean inRange = PickpocketUtil.inRangeOfBackpack(player, mc.player);
             float lineRed = inRange ? 0.0F : 1.0F;
             float lineGreen = inRange ? 1.0F : 1.0F;
             float lineBlue = inRange ? 0.0F : 1.0F;
             Matrix4f matrix4f = stack.last().pose();
             Vector3d pos = player.getPosition(event.getPartialTicks());
-            Vector3d start = Vector3d.directionFromRotation(0, player.yBodyRot + 180 - Config.SERVER.pickpocketMaxRangeAngle.get().floatValue()).scale(Config.SERVER.pickpocketMaxReachDistance.get());
-            Vector3d end = Vector3d.directionFromRotation(0, player.yBodyRot - 180 + Config.SERVER.pickpocketMaxRangeAngle.get().floatValue()).scale(Config.SERVER.pickpocketMaxReachDistance.get());
+            Vector3d start = Vector3d.directionFromRotation(0, bodyRotation + 180 - Config.SERVER.pickpocketMaxRangeAngle.get().floatValue()).scale(Config.SERVER.pickpocketMaxReachDistance.get());
+            Vector3d end = Vector3d.directionFromRotation(0, bodyRotation - 180 + Config.SERVER.pickpocketMaxRangeAngle.get().floatValue()).scale(Config.SERVER.pickpocketMaxReachDistance.get());
             builder.vertex(matrix4f, (float) (pos.x + start.x),(float) (pos.y + start.y), (float) (pos.z + start.z)).color(lineRed, lineGreen, lineBlue, 1.0F).endVertex();
             builder.vertex(matrix4f, (float) pos.x,(float) pos.y, (float) pos.z).color(lineRed, lineGreen, lineBlue, 1.0F).endVertex();
             builder.vertex(matrix4f, (float) (pos.x + end.x),(float) (pos.y + end.y), (float) (pos.z + end.z)).color(lineRed, lineGreen, lineBlue, 1.0F).endVertex();
