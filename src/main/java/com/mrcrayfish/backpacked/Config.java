2d8bddad00f353f261cd4a43bd72da9a567839a8
MrCrayfish <mrcrayfish@hotmail.com>
2022-03-15 18:47:20 +1030

    🐛 Fixed incorrectly named config option

--- src/main/java/com/mrcrayfish/backpacked/Config.java
+++ src/main/java/com/mrcrayfish/backpacked/Config.java
@@ -34,7 +34,7 @@ public class Config
         public final ForgeConfigSpec.LongValue wanderingTraderForgetTime;
         public final ForgeConfigSpec.BooleanValue dislikedPlayersCanTrade;
         public final ForgeConfigSpec.IntValue dislikeCooldown;
-        public final ForgeConfigSpec.IntValue maxLootMultipler;
+        public final ForgeConfigSpec.IntValue maxLootMultiplier;
         public final ForgeConfigSpec.IntValue maxEmeraldStack;
 
         Common(ForgeConfigSpec.Builder builder)
@@ -77,10 +77,10 @@ public class Config
                     .comment("The amount of time (in ticks) a player has to wait before a Wandering Trader will like them again. If a player gets caught pickpocketing a Wandering Trader, the cooldown will be reset")
                     .translation("backpacked.configgui.dislikeCooldown")
                     .defineInRange("dislikeCooldown", 6000, 0, 24000);
-            this.maxLootMultipler = builder
+            this.maxLootMultiplier = builder
                     .comment("The maximum multiplier to apply when generating loot in the Wandering Trader backpack")
                     .translation("backpacked.configgui.maxEmeraldStack")
-                    .defineInRange("maxEmeraldStack", 12, 1, 64);
+                    .defineInRange("maxLootMultiplier", 12, 1, 64);
             this.maxEmeraldStack = builder
                     .comment("The maximum size of an emerald stack that can generate in the Wandering Trader backpack")
                     .translation("backpacked.configgui.maxEmeraldStack")
