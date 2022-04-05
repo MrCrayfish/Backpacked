957ac234e00d28a0697a13c63e5a15615a707281
MrCrayfish <mrcrayfish@hotmail.com>
2022-04-05 20:24:26 +0930

    🐛 Fixed unlocked backpacks not synced after dimension change

--- src/main/java/com/mrcrayfish/backpacked/common/UnlockTracker.java
+++ src/main/java/com/mrcrayfish/backpacked/common/UnlockTracker.java
@@ -163,6 +163,14 @@ public class UnlockTracker
     }
 
     @SubscribeEvent
+    public static void onPlayerChangedDimension(PlayerEvent.PlayerChangedDimensionEvent event)
+    {
+        get(event.getPlayer()).ifPresent(unlockTracker -> {
+            Network.getPlayChannel().send(PacketDistributor.PLAYER.with(() -> (ServerPlayer) event.getPlayer()), new MessageSyncUnlockTracker(unlockTracker.getUnlockedBackpacks()));
+        });
+    }
+
+    @SubscribeEvent
     public static void onServerTick(TickEvent.ServerTickEvent event)
     {
         if(event.phase != TickEvent.Phase.END)
