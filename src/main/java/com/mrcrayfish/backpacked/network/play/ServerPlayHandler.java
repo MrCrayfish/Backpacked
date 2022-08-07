733732a655b456e38aa4c290a5b7591757d821cc
MrCrayfish <mrcrayfish@hotmail.com>
2022-08-07 21:37:23 +0930

    🐛 Fixed pickpocket setting not working as intended

--- src/main/java/com/mrcrayfish/backpacked/network/play/ServerPlayHandler.java
+++ src/main/java/com/mrcrayfish/backpacked/network/play/ServerPlayHandler.java
@@ -74,11 +74,11 @@ public class ServerPlayHandler
 
     public static void handleEntityBackpack(MessageEntityBackpack message, ServerPlayer player)
     {
-        if(!Config.SERVER.pickpocketBackpacks.get())
-            return;
-
         Entity entity = player.level.getEntity(message.getEntityId());
         if(!(entity instanceof LivingEntity otherEntity))
+            return;
+
+        if(otherEntity instanceof ServerPlayer && !Config.SERVER.pickpocketBackpacks.get())
             return;
 
         if(!PickpocketUtil.canSeeBackpack(otherEntity, player))
