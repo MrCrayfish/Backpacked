02f4838e9253685e7ab9a379c568f19acc970e42
MrCrayfish <mrcrayfish@hotmail.com>
2023-04-28 17:41:44 +0930

    🐛 Fixed crash with latest Controllable update

--- src/main/java/com/mrcrayfish/backpacked/BackpackedButtonBindings.java
+++ src/main/java/com/mrcrayfish/backpacked/BackpackedButtonBindings.java
@@ -1,5 +1,6 @@
 package com.mrcrayfish.backpacked;
 
+import com.mrcrayfish.controllable.client.BindingContext;
 import com.mrcrayfish.controllable.client.BindingRegistry;
 import com.mrcrayfish.controllable.client.ButtonBinding;
 import net.minecraftforge.client.settings.KeyConflictContext;
@@ -9,7 +10,7 @@ import net.minecraftforge.client.settings.KeyConflictContext;
  */
 public class BackpackedButtonBindings
 {
-    public static final ButtonBinding BACKPACK = new ButtonBinding(-1, "backpacked.button.open_backpack", "button.categories.backpacked", KeyConflictContext.IN_GAME);
+    public static final ButtonBinding BACKPACK = new ButtonBinding(-1, "backpacked.button.open_backpack", "button.categories.backpacked", BindingContext.IN_GAME);
 
     public static void register()
     {
