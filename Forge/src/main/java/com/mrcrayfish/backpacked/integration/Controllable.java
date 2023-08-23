ce4ad4f993625de1f8c234e965cccea34e2cc0a5
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2023-08-23 11:27:25 +0930

    ⬆ Updated Controllable to latest version

--- Forge/src/main/java/com/mrcrayfish/backpacked/integration/Controllable.java
+++ Forge/src/main/java/com/mrcrayfish/backpacked/integration/Controllable.java
@@ -3,13 +3,13 @@ package com.mrcrayfish.backpacked.integration;
 import com.mrcrayfish.backpacked.network.Network;
 import com.mrcrayfish.backpacked.network.message.MessageOpenBackpack;
 import com.mrcrayfish.backpacked.platform.Services;
+import com.mrcrayfish.controllable.client.BindingContext;
 import com.mrcrayfish.controllable.client.BindingRegistry;
 import com.mrcrayfish.controllable.client.ButtonBinding;
 import com.mrcrayfish.controllable.event.ControllerEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -18,7 +18,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
  */
 public class Controllable
 {
-    public static final ButtonBinding BACKPACK_BUTTON = new ButtonBinding(-1, "backpacked.button.open_backpack", "button.categories.backpacked", KeyConflictContext.IN_GAME);
+    public static final ButtonBinding BACKPACK_BUTTON = new ButtonBinding(-1, "backpacked.button.open_backpack", "button.categories.backpacked", BindingContext.IN_GAME);
 
     public static void init()
     {
