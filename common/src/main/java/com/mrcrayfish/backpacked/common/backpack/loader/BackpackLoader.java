894450fa9093528fef31a52d55b89cf4ff6f4ebd
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-05-21 01:01:08 +0930

    📦 Just dump exception if backpack fails to load

--- common/src/main/java/com/mrcrayfish/backpacked/common/backpack/loader/BackpackLoader.java
+++ common/src/main/java/com/mrcrayfish/backpacked/common/backpack/loader/BackpackLoader.java
@@ -3,11 +3,13 @@ package com.mrcrayfish.backpacked.common.backpack.loader;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
 import com.mrcrayfish.backpacked.Constants;
 import com.mrcrayfish.backpacked.common.backpack.Backpack;
 import com.mrcrayfish.backpacked.common.backpack.BackpackManager;
+import net.minecraft.Util;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
@@ -35,14 +37,9 @@ public class BackpackLoader extends SimpleJsonResourceReloadListener
         // TODO check for order
         Map<ResourceLocation, Backpack> backpacks = new HashMap<>();
         map.forEach((location, element) -> {
-            DataResult<Backpack> result = Backpack.CODEC.parse(JsonOps.INSTANCE, element);
-            if(result.error().isPresent()) {
-                Constants.LOG.error("Failed to parse result json: " + location);
-            } else {
-                Backpack backpack = result.getOrThrow(false, Constants.LOG::error);
-                backpack.setup(location);
-                backpacks.put(location, backpack);
-            }
+            Backpack backpack = Util.getOrThrow(Backpack.CODEC.parse(JsonOps.INSTANCE, element), JsonParseException::new);
+            backpack.setup(location);
+            backpacks.put(location, backpack);
         });
         BackpackManager.instance().updateBackpacks(backpacks);
     }
