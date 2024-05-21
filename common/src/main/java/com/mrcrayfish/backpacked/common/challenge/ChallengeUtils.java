242a1e734db954d0980c6729174da83163c7a782
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-05-22 02:13:49 +0930

    📦 Changed all challenges to use predicate versions for codecs

---
+++ common/src/main/java/com/mrcrayfish/backpacked/common/challenge/ChallengeUtils.java
@@ -0,0 +1,88 @@
+package com.mrcrayfish.backpacked.common.challenge;
+
+import com.mrcrayfish.backpacked.Constants;
+import net.minecraft.advancements.critereon.BlockPredicate;
+import net.minecraft.advancements.critereon.EntityPredicate;
+import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.nbt.NbtAccounter;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.Optional;
+
+/**
+ * Author: MrCrayfish
+ */
+@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+public final class ChallengeUtils
+{
+    public static boolean testPredicate(Optional<BlockPredicate> optional, BlockState state)
+    {
+        if(optional.isEmpty())
+            return true;
+        BlockPredicate predicate = optional.get();
+        if(predicate.tag().isPresent() && !state.is(predicate.tag().get()))
+            return false;
+        if(predicate.blocks().isPresent() && !state.is(predicate.blocks().get()))
+            return false;
+        return predicate.properties().isEmpty() || predicate.properties().get().matches(state);
+    }
+
+    public static boolean testPredicate(Optional<ItemPredicate> optional, ItemStack stack)
+    {
+        if(optional.isEmpty())
+            return true;
+        ItemPredicate predicate = optional.get();
+        return predicate.matches(stack);
+    }
+
+    public static void writeBlockPredicate(FriendlyByteBuf buf, Optional<BlockPredicate> optional)
+    {
+        buf.writeOptional(optional, (buf1, predicate) -> {
+            buf1.writeNbt(BlockPredicate.CODEC.encodeStart(NbtOps.INSTANCE, predicate)
+                .getOrThrow(false, Constants.LOG::error));
+        });
+    }
+
+    public static Optional<BlockPredicate> readBlockPredicate(FriendlyByteBuf buf)
+    {
+        return buf.readOptional(buf1 -> BlockPredicate.CODEC
+            .parse(NbtOps.INSTANCE, buf1.readNbt(NbtAccounter.create(2097152L)))
+            .getOrThrow(false, Constants.LOG::error)
+        );
+    }
+
+    public static void writeItemPredicate(FriendlyByteBuf buf, Optional<ItemPredicate> optional)
+    {
+        buf.writeOptional(optional, (buf1, predicate) -> {
+            buf1.writeNbt(ItemPredicate.CODEC.encodeStart(NbtOps.INSTANCE, predicate)
+                .getOrThrow(false, Constants.LOG::error));
+        });
+    }
+
+    public static Optional<ItemPredicate> readItemPredicate(FriendlyByteBuf buf)
+    {
+        return buf.readOptional(buf1 -> ItemPredicate.CODEC
+            .parse(NbtOps.INSTANCE, buf1.readNbt(NbtAccounter.create(2097152L)))
+            .getOrThrow(false, Constants.LOG::error)
+        );
+    }
+
+    public static void writeEntityPredicate(FriendlyByteBuf buf, Optional<EntityPredicate> optional)
+    {
+        buf.writeOptional(optional, (buf1, predicate) -> {
+            buf1.writeNbt(EntityPredicate.CODEC.encodeStart(NbtOps.INSTANCE, predicate)
+                .getOrThrow(false, Constants.LOG::error));
+        });
+    }
+
+    public static Optional<EntityPredicate> readEntityPredicate(FriendlyByteBuf buf)
+    {
+        return buf.readOptional(buf1 -> EntityPredicate.CODEC
+            .parse(NbtOps.INSTANCE, buf1.readNbt(NbtAccounter.create(2097152L)))
+            .getOrThrow(false, Constants.LOG::error)
+        );
+    }
+}
--- common/src/main/java/com/mrcrayfish/backpacked/common/challenge/impl/InteractWithBlockChallenge.java
+++ common/src/main/java/com/mrcrayfish/backpacked/common/challenge/impl/InteractWithBlockChallenge.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.mrcrayfish.backpacked.Constants;
 import com.mrcrayfish.backpacked.common.challenge.Challenge;
 import com.mrcrayfish.backpacked.common.challenge.ChallengeSerializer;
+import com.mrcrayfish.backpacked.common.challenge.ChallengeUtils;
 import com.mrcrayfish.backpacked.common.tracker.IProgressTracker;
 import com.mrcrayfish.backpacked.common.tracker.ProgressFormatters;
 import com.mrcrayfish.backpacked.common.tracker.impl.CountProgressTracker;
@@ -12,13 +13,9 @@ import com.mrcrayfish.backpacked.data.unlock.UnlockManager;
 import com.mrcrayfish.backpacked.event.EventType;
 import net.minecraft.advancements.critereon.BlockPredicate;
 import net.minecraft.advancements.critereon.ItemPredicate;
-import net.minecraft.nbt.NbtAccounter;
-import net.minecraft.nbt.NbtOps;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ExtraCodecs;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.Optional;
 
@@ -31,7 +28,7 @@ public class InteractWithBlockChallenge extends Challenge
     public static final ResourceLocation ID = new ResourceLocation(Constants.MOD_ID, "interact_with_block");
     public static final Serializer SERIALIZER = new Serializer();
     public static final Codec<InteractWithBlockChallenge> CODEC = RecordCodecBuilder.create(builder -> {
-        return builder.group(BlockPredicate.CODEC.optionalFieldOf("block").forGetter(challenge -> {
+        return builder.group(BlockPredicate.CODEC.optionalFieldOf("target").forGetter(challenge -> {
             return challenge.block;
         }), ItemPredicate.CODEC.optionalFieldOf("using_item").forGetter(challenge -> {
             return challenge.item;
@@ -44,7 +41,7 @@ public class InteractWithBlockChallenge extends Challenge
     private final Optional<ItemPredicate> item;
     private final int count;
 
-    protected InteractWithBlockChallenge(Optional<BlockPredicate> block, Optional<ItemPredicate> item, int count)
+    public InteractWithBlockChallenge(Optional<BlockPredicate> block, Optional<ItemPredicate> item, int count)
     {
         super(ID);
         this.block = block;
@@ -69,28 +66,16 @@ public class InteractWithBlockChallenge extends Challenge
         @Override
         public void write(InteractWithBlockChallenge challenge, FriendlyByteBuf buf)
         {
-            buf.writeOptional(challenge.block, (buf1, predicate) -> {
-                buf1.writeNbt(BlockPredicate.CODEC.encodeStart(NbtOps.INSTANCE, predicate)
-                    .getOrThrow(false, Constants.LOG::error));
-            });
-            buf.writeOptional(challenge.item, (buf1, predicate) -> {
-                buf1.writeNbt(ItemPredicate.CODEC.encodeStart(NbtOps.INSTANCE, predicate)
-                    .getOrThrow(false, Constants.LOG::error));
-            });
+            ChallengeUtils.writeBlockPredicate(buf, challenge.block);
+            ChallengeUtils.writeItemPredicate(buf, challenge.item);
             buf.writeVarInt(challenge.count);
         }
 
         @Override
         public InteractWithBlockChallenge read(FriendlyByteBuf buf)
         {
-            Optional<BlockPredicate> block = buf.readOptional(buf1 -> {
-                return BlockPredicate.CODEC.parse(NbtOps.INSTANCE, buf1.readNbt(NbtAccounter.create(2097152L)))
-                    .getOrThrow(false, Constants.LOG::error);
-            });
-            Optional<ItemPredicate> item = buf.readOptional(buf1 -> {
-                return ItemPredicate.CODEC.parse(NbtOps.INSTANCE, buf1.readNbt(NbtAccounter.create(2097152L)))
-                    .getOrThrow(false, Constants.LOG::error);
-            });
+            Optional<BlockPredicate> block = ChallengeUtils.readBlockPredicate(buf);
+            Optional<ItemPredicate> item = ChallengeUtils.readItemPredicate(buf);
             int count = buf.readVarInt();
             return new InteractWithBlockChallenge(block, item, count);
         }
@@ -104,38 +89,18 @@ public class InteractWithBlockChallenge extends Challenge
 
     protected static class Tracker extends CountProgressTracker
     {
-        private final Optional<BlockPredicate> blockPredicate;
-        private final Optional<ItemPredicate> itemPredicate;
-
         protected Tracker(int maxCount, Optional<BlockPredicate> block, Optional<ItemPredicate> item)
         {
             super(maxCount, ProgressFormatters.MINED_X_OF_X);
-            this.blockPredicate = block;
-            this.itemPredicate = item;
             UnlockManager.instance().addEventListener(EventType.USED_ITEM_ON_BLOCK, (state, stack, player) -> {
                 if(this.isComplete() || player.level().isClientSide())
                     return;
-                if(this.testState(state) && this.testItem(stack)) {
-                    this.increment(player);
+                if(ChallengeUtils.testPredicate(block, state)) {
+                    if(ChallengeUtils.testPredicate(item, stack)) {
+                        this.increment(player);
+                    }
                 }
             });
-        }
-
-        private boolean testState(BlockState state)
-        {
-            if(this.blockPredicate.isEmpty())
-                return true;
-            BlockPredicate predicate = this.blockPredicate.get();
-            if(predicate.tag().isPresent() && !state.is(predicate.tag().get()))
-                return false;
-            if(predicate.blocks().isPresent() && !state.is(predicate.blocks().get()))
-                return false;
-            return predicate.properties().isEmpty() || predicate.properties().get().matches(state);
-        }
-
-        private boolean testItem(ItemStack stack)
-        {
-            return this.itemPredicate.map(predicate -> predicate.matches(stack)).orElse(true);
         }
     }
 }
