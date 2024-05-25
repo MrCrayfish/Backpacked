75d1b5d8467b7d296a1a94ba983bea0060af6408
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-05-26 03:48:17 +0930

    📦 Updated unlock backpack toast

--- common/src/main/java/com/mrcrayfish/backpacked/client/gui/toasts/UnlockBackpackToast.java
+++ common/src/main/java/com/mrcrayfish/backpacked/client/gui/toasts/UnlockBackpackToast.java
@@ -1,24 +1,32 @@
 package com.mrcrayfish.backpacked.client.gui.toasts;
 
+import com.mrcrayfish.backpacked.client.gui.screen.CustomiseBackpackScreen;
 import com.mrcrayfish.backpacked.common.backpack.Backpack;
+import com.mrcrayfish.backpacked.core.ModItems;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 /**
  * Author: MrCrayfish
  */
 public class UnlockBackpackToast implements Toast
 {
+    private static final ResourceLocation BACKGROUND_SPRITE = new ResourceLocation("toast/recipe");
     private static final Component TITLE = Component.translatable("backpacked.toast.unlocked_backpack").withStyle(ChatFormatting.YELLOW);
 
+    private final Backpack backpack;
     private final Component name;
 
     public UnlockBackpackToast(Backpack backpack)
     {
-        this.name = Component.translatable(backpack.getId().getNamespace() + ".backpack." + backpack.getId().getPath());
+        this.backpack = backpack;
+        this.name = Component.translatable(backpack.getTranslationKey()).withStyle(ChatFormatting.DARK_GRAY);
     }
 
     @Override
@@ -27,8 +35,7 @@ public class UnlockBackpackToast implements Toast
         graphics.blit(TEXTURE, 0, 0, 0, 0, 160, 32);
         graphics.drawString(gui.getMinecraft().font, TITLE, 35, 7, 0xFFFFFF, false);
         graphics.drawString(gui.getMinecraft().font, this.name, 35, 18, 0xFFFFFF, false);
-        // TODO fix this
-        //CustomiseBackpackScreen.drawBackpackModel(graphics, this.model, 15, 7, 30F, 0, 0F);
+        CustomiseBackpackScreen.drawBackpackInGui(Minecraft.getInstance(), graphics, new ItemStack(ModItems.BACKPACK.get()), this.backpack, 18, 16, 0);
         return delta >= 5000L ? Visibility.HIDE : Visibility.SHOW;
     }
 }
