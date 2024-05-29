65937682a1cec5d5a35c5a9165c3c6dc031847f1
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-05-29 16:37:40 +0930

    📦 Enchantments now use custom category on Fabric

--- fabric/src/main/java/com/mrcrayfish/backpacked/Backpacked.java
+++ fabric/src/main/java/com/mrcrayfish/backpacked/Backpacked.java
@@ -1,6 +1,10 @@
 package com.mrcrayfish.backpacked;
 
+import com.chocohead.mm.api.ClassTinkerers;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
 import com.mrcrayfish.backpacked.common.WanderingTraderEvents;
+import com.mrcrayfish.backpacked.item.BackpackItem;
 import com.mrcrayfish.framework.FrameworkSetup;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
@@ -8,9 +12,12 @@ import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.fabricmc.fabric.impl.event.interaction.InteractionEventsRouter;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
 public class Backpacked implements ModInitializer
 {
+    public static final Supplier<EnchantmentCategory> ENCHANTMENT_TYPE = Suppliers.memoize(() -> ClassTinkerers.getEnum(EnchantmentCategory.class, "BACKPACKED$BACKPACK"));
+
     private static boolean trinketsLoaded;
 
     public Backpacked()
---
+++ fabric/src/main/java/com/mrcrayfish/backpacked/mixin/EnchantmentCategoryMixin.java
@@ -0,0 +1,16 @@
+package com.mrcrayfish.backpacked.mixin;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mixin(EnchantmentCategory.class)
+public abstract class EnchantmentCategoryMixin
+{
+    @Shadow
+    public abstract boolean canEnchant(Item item);
+}
