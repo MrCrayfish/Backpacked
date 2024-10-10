d99f2cf93f78aa9f686cc3131a7884773857636e
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-10-10 16:42:13 +1030

    📦 Removed old trinkets code

--- fabric/src/main/java/com/mrcrayfish/backpacked/platform/FabricBackpackHelper.java
+++ fabric/src/main/java/com/mrcrayfish/backpacked/platform/FabricBackpackHelper.java
@@ -1,22 +1,18 @@
 package com.mrcrayfish.backpacked.platform;
 
 import com.mrcrayfish.backpacked.blockentity.ShelfBlockEntity;
-import com.mrcrayfish.backpacked.integration.Trinkets;
+import com.mrcrayfish.backpacked.integration.AccessoriesHelper;
 import com.mrcrayfish.backpacked.inventory.container.BackpackContainerMenu;
 import com.mrcrayfish.backpacked.inventory.container.data.BackpackContainerData;
 import com.mrcrayfish.backpacked.item.BackpackItem;
 import com.mrcrayfish.backpacked.platform.services.IBackpackHelper;
 import com.mrcrayfish.framework.api.FrameworkAPI;
-import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
 import net.minecraft.core.BlockPos;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleMenuProvider;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
@@ -29,7 +25,7 @@ public class FabricBackpackHelper implements IBackpackHelper
     @Override
     public ItemStack getBackpackStack(Player player)
     {
-        return Trinkets.getBackpackStack(player);
+        return AccessoriesHelper.getBackpackStack(player);
     }
 
     @Override
@@ -37,7 +33,7 @@ public class FabricBackpackHelper implements IBackpackHelper
     {
         if(!(stack.getItem() instanceof BackpackItem) && !stack.isEmpty())
             return false;
-        Trinkets.setBackpackStack(player, stack);
+        AccessoriesHelper.setBackpackStack(player, stack);
         return true;
     }
 
@@ -64,6 +60,6 @@ public class FabricBackpackHelper implements IBackpackHelper
     @Override
     public BackpackItem createBackpackItem(Item.Properties properties)
     {
-        return Trinkets.createTrinketBackpack(properties);
+        return new BackpackItem(properties);
     }
 }
