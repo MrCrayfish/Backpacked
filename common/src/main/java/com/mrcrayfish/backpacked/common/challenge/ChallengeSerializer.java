37cd478f3ab4d80dcab16c694656327ff8ee4c18
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-04-26 14:40:23 +0930

    📦 Improved error message when deserializing challenges

--- common/src/main/java/com/mrcrayfish/backpacked/common/challenge/ChallengeSerializer.java
+++ common/src/main/java/com/mrcrayfish/backpacked/common/challenge/ChallengeSerializer.java
@@ -12,9 +12,9 @@ public record ChallengeSerializer<T extends Challenge>(ResourceLocation id, MapC
 {
     public static final Codec<ChallengeSerializer<?>> CODEC = ResourceLocation.CODEC.flatXmap(id -> {
         ChallengeSerializer<?> serializer = ChallengeManager.instance().getSerializer(id);
-        return serializer != null ? DataResult.success(serializer) : DataResult.error(() -> "Serializer does not exist");
+        return serializer != null ? DataResult.success(serializer) : DataResult.error(() -> "The challenge type '" + id + "' does not exist");
     }, serializer -> {
         ResourceLocation id = ChallengeManager.instance().getSerializerId(serializer);
-        return id != null ? DataResult.success(id) : DataResult.error(() -> "Unregistered serializer");
+        return id != null ? DataResult.success(id) : DataResult.error(() -> "Unregistered challenge type: " + serializer.id);
     });
 }
