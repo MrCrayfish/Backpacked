8cfaa72ca4e35af065644e6f262644a8656e0a39
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-05-03 13:16:59 +0930

    📦 Add module and power operators

--- common/src/main/java/com/mrcrayfish/backpacked/client/renderer/backpack/advanced/value/Operator.java
+++ common/src/main/java/com/mrcrayfish/backpacked/client/renderer/backpack/advanced/value/Operator.java
@@ -1,6 +1,7 @@
 package com.mrcrayfish.backpacked.client.renderer.backpack.advanced.value;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.util.Mth;
 import net.minecraft.util.StringRepresentable;
 
 import java.util.Locale;
@@ -8,21 +9,26 @@ import java.util.function.BiFunction;
 
 public enum Operator implements StringRepresentable
 {
-    FIRST((a, b) -> a),
-    LAST((a, b) -> b),
-    ADD(Double::sum),
-    SUBTRACT((a, b) -> a - b),
-    MULTIPLY((a, b) -> a * b),
-    DIVIDE((a, b) -> a / b),
-    MIN(Math::min),
-    MAX(Math::max);
+    FIRST("first", (a, b) -> a),
+    LAST("last", (a, b) -> b),
+    ADD("add", Double::sum),
+    SUBTRACT("subtract", (a, b) -> a - b),
+    MULTIPLY("multiply", (a, b) -> a * b),
+    DIVIDE("divide", (a, b) -> a / b),
+    MODULO("modulo", (a, b) -> a % b),
+    POSITIVE_MODULO("positive_modulo", Mth::positiveModulo),
+    MIN("min", Math::min),
+    MAX("max", Math::max),
+    POWER("power", Math::pow);
 
     public static final Codec<Operator> CODEC = StringRepresentable.fromEnum(Operator::values);
 
+    private final String name;
     private final BiFunction<Double, Double, Double> function;
 
-    Operator(BiFunction<Double, Double, Double> function)
+    Operator(String name, BiFunction<Double, Double, Double> function)
     {
+        this.name = name;
         this.function = function;
     }
 
@@ -34,6 +40,6 @@ public enum Operator implements StringRepresentable
     @Override
     public String getSerializedName()
     {
-        return this.name().toLowerCase(Locale.ENGLISH);
+        return this.name;
     }
 }
