5dbb6fa61c2925a4c02562bccc554822db521bc9
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-05-04 23:02:58 +0930

    ✨ Added light level to entity data value

--- common/src/main/java/com/mrcrayfish/backpacked/client/renderer/backpack/advanced/value/EntityDataValue.java
+++ common/src/main/java/com/mrcrayfish/backpacked/client/renderer/backpack/advanced/value/EntityDataValue.java
@@ -36,35 +36,36 @@ public record EntityDataValue(Data data) implements Value
 
     private enum Data implements StringRepresentable
     {
-        WALK_POSITION("walk_position", (entity, context) -> entity.walkAnimation.position(context.partialTick())),
-        WALK_SPEED("walk_speed", (entity, context) -> entity.walkAnimation.speed(context.partialTick())),
-        HEAD_YAW("head_yaw", (entity, context) -> entity.getViewXRot(context.partialTick())),
-        HEAD_PITCH("head_pitch", (entity, context) -> entity.getViewYRot(context.partialTick())),
-        BODY_ROTATION("body_rotation", (entity, context) -> Mth.rotLerp(context.partialTick(), entity.yBodyRotO, entity.yBodyRot)),
-        SWIM_ANGLE("swimming", (entity, context) -> entity.getSwimAmount(context.partialTick())),
-        ATTACK_SWING("attack_swing", (entity, context) -> entity.getAttackAnim(context.partialTick())),
-        FALL_FLYING_COUNTER("fall_flying_counter", (entity, context) -> entity.isFallFlying() ? (float) entity.getFallFlyingTicks() + context.partialTick() : 0F),
-        HEALTH("health", (entity, context) -> entity.getHealth()),
-        MAX_HEALTH("max_health", (entity, context) -> entity.getMaxHealth()),
-        ABSORPTION("absorption", (entity, context) -> entity.getAbsorptionAmount()),
-        MAX_ABSORPTION("max_absorption", (entity, context) -> entity.getMaxAbsorption()),
-        ARMOR("armor", (entity, context) -> (float) entity.getArmorValue()),
-        ARMOR_COVERAGE("armor_coverage", (entity, context) -> entity.getArmorCoverPercentage()),
-        AIR_SUPPLY("air_supply", (entity, context) -> (float) entity.getAirSupply()),
-        MAX_AIR_SUPPLY("max_air_supply", (entity, context) -> (float) entity.getMaxAirSupply()),
-        POSITION_X("pos_x", (entity, context) -> (float) Mth.lerp(context.partialTick(), entity.xo, entity.getX())),
-        POSITION_Y("pos_y", (entity, context) -> (float) Mth.lerp(context.partialTick(), entity.yo, entity.getY())),
-        POSITION_Z("pos_z", (entity, context) -> (float) Mth.lerp(context.partialTick(), entity.zo, entity.getZ())),
-        MOTION_X("motion_x", (entity, context) -> (float) entity.getDeltaMovement().x),
-        MOTION_Y("motion_y", (entity, context) -> (float) entity.getDeltaMovement().y),
-        MOTION_Z("motion_z", (entity, context) -> (float) entity.getDeltaMovement().z);
+        WALK_POSITION("walk_position", (entity, context) -> (double) entity.walkAnimation.position(context.partialTick())),
+        WALK_SPEED("walk_speed", (entity, context) -> (double) entity.walkAnimation.speed(context.partialTick())),
+        HEAD_YAW("head_yaw", (entity, context) -> (double) entity.getViewXRot(context.partialTick())),
+        HEAD_PITCH("head_pitch", (entity, context) -> (double) entity.getViewYRot(context.partialTick())),
+        BODY_ROTATION("body_rotation", (entity, context) -> (double) Mth.rotLerp(context.partialTick(), entity.yBodyRotO, entity.yBodyRot)),
+        SWIM_ANGLE("swimming", (entity, context) -> (double) entity.getSwimAmount(context.partialTick())),
+        ATTACK_SWING("attack_swing", (entity, context) -> (double) entity.getAttackAnim(context.partialTick())),
+        FALL_FLYING_COUNTER("fall_flying_counter", (entity, context) -> entity.isFallFlying() ? (double) entity.getFallFlyingTicks() + context.partialTick() : 0),
+        HEALTH("health", (entity, context) -> (double) entity.getHealth()),
+        MAX_HEALTH("max_health", (entity, context) -> (double) entity.getMaxHealth()),
+        ABSORPTION("absorption", (entity, context) -> (double) entity.getAbsorptionAmount()),
+        MAX_ABSORPTION("max_absorption", (entity, context) -> (double) entity.getMaxAbsorption()),
+        ARMOR("armor", (entity, context) -> (double) entity.getArmorValue()),
+        ARMOR_COVERAGE("armor_coverage", (entity, context) -> (double) entity.getArmorCoverPercentage()),
+        AIR_SUPPLY("air_supply", (entity, context) -> (double) entity.getAirSupply()),
+        MAX_AIR_SUPPLY("max_air_supply", (entity, context) -> (double) entity.getMaxAirSupply()),
+        POSITION_X("pos_x", (entity, context) -> Mth.lerp(context.partialTick(), entity.xo, entity.getX())),
+        POSITION_Y("pos_y", (entity, context) -> Mth.lerp(context.partialTick(), entity.yo, entity.getY())),
+        POSITION_Z("pos_z", (entity, context) -> Mth.lerp(context.partialTick(), entity.zo, entity.getZ())),
+        MOTION_X("motion_x", (entity, context) -> entity.getDeltaMovement().x),
+        MOTION_Y("motion_y", (entity, context) -> entity.getDeltaMovement().y),
+        MOTION_Z("motion_z", (entity, context) -> entity.getDeltaMovement().z),
+        LIGHT_LEVEL("light_level", (entity, context) -> (double) entity.level().getLightEmission(entity.blockPosition()));
 
         public static final Codec<Data> CODEC = StringRepresentable.fromEnum(Data::values);
 
         private final String name;
-        private final BiFunction<LivingEntity, BackpackRenderContext, Float> function;
+        private final BiFunction<LivingEntity, BackpackRenderContext, Double> function;
 
-        Data(String name, BiFunction<LivingEntity, BackpackRenderContext, Float> function)
+        Data(String name, BiFunction<LivingEntity, BackpackRenderContext, Double> function)
         {
             this.name = name;
             this.function = function;
