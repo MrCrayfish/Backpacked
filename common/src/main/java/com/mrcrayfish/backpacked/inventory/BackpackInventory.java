c7d78708390b17d2e0d290d01aa74af562d8c03c
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-07-22 17:56:11 +0930

    📦 Keep track of state for better menu validation

--- common/src/main/java/com/mrcrayfish/backpacked/inventory/BackpackInventory.java
+++ common/src/main/java/com/mrcrayfish/backpacked/inventory/BackpackInventory.java
@@ -15,16 +15,22 @@ import net.minecraft.world.item.component.ItemContainerContents;
 public class BackpackInventory extends SimpleContainer
 {
     private final int index;
+    private final int columns;
+    private final int rows;
     private final Player player;
     private final ItemStack stack;
+    private final boolean slotsUnlocked;
     private boolean save;
 
     public BackpackInventory(int index, int columns, int rows, Player player, ItemStack stack)
     {
         super(rows * columns);
         this.index = index;
+        this.columns = columns;
+        this.rows = rows;
         this.player = player;
         this.stack = stack;
+        this.slotsUnlocked = Config.SERVER.backpack.inventory.slots.unlockAll.get();
         this.loadBackpackContents(player);
     }
 
@@ -43,7 +49,17 @@ public class BackpackInventory extends SimpleContainer
     @Override
     public boolean stillValid(Player player)
     {
-        return this.player.isAlive() && (BackpackHelper.getBackpackStack(this.player, this.index).equals(this.stack) && (this.player.equals(player) || PickpocketUtil.canPickpocketEntity(this.player, player, Config.SERVER.pickpocketing.maxReachDistance.get() + 0.5)));
+        if(!this.player.isAlive())
+            return false;
+        if(this.columns != Config.SERVER.backpack.inventory.size.columns.get())
+            return false;
+        if(this.rows != Config.SERVER.backpack.inventory.size.rows.get())
+            return false;
+        if(this.slotsUnlocked != Config.SERVER.backpack.inventory.slots.unlockAll.get())
+            return false;
+        if(!BackpackHelper.getBackpackStack(this.player, this.index).equals(this.stack))
+            return false;
+        return this.player.equals(player) || PickpocketUtil.canPickpocketEntity(this.player, player, Config.SERVER.pickpocketing.maxReachDistance.get() + 0.5);
     }
 
     @Override
