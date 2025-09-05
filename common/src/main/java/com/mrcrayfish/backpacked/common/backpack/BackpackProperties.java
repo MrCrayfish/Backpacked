323ae958878cd4082dc6be686e65b1fc2363646a
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-09-05 15:26:31 +0930

    📦 Remove enchantment glint option

--- common/src/main/java/com/mrcrayfish/backpacked/common/backpack/BackpackProperties.java
+++ common/src/main/java/com/mrcrayfish/backpacked/common/backpack/BackpackProperties.java
@@ -12,42 +12,35 @@ import java.util.Optional;
 /**
  * Author: MrCrayfish
  */
-public record BackpackProperties(Optional<ResourceLocation> cosmetic, boolean showWithElytra, boolean showEffects, boolean showEnchantmentGlint)
+public record BackpackProperties(Optional<ResourceLocation> cosmetic, boolean showWithElytra, boolean showEffects)
 {
-    public static final BackpackProperties DEFAULT = new BackpackProperties(Optional.empty(), false, true, false);
+    public static final BackpackProperties DEFAULT = new BackpackProperties(Optional.empty(), false, true);
 
     public static final Codec<BackpackProperties> CODEC = RecordCodecBuilder.create(builder -> builder.group(
         ResourceLocation.CODEC.optionalFieldOf("cosmetic").forGetter(o -> o.cosmetic),
         Codec.BOOL.fieldOf("show_with_elytra").forGetter(o -> o.showWithElytra),
-        Codec.BOOL.fieldOf("show_effects").forGetter(o -> o.showEffects),
-        Codec.BOOL.fieldOf("show_enchantment_glint").forGetter(o -> o.showEnchantmentGlint)
+        Codec.BOOL.fieldOf("show_effects").forGetter(o -> o.showEffects)
     ).apply(builder, BackpackProperties::new));
 
     public static final StreamCodec<RegistryFriendlyByteBuf, BackpackProperties> STREAM_CODEC = StreamCodec.composite(
         ResourceLocation.STREAM_CODEC.apply(ByteBufCodecs::optional), BackpackProperties::cosmetic,
         ByteBufCodecs.BOOL, BackpackProperties::showWithElytra,
         ByteBufCodecs.BOOL, BackpackProperties::showEffects,
-        ByteBufCodecs.BOOL, BackpackProperties::showEnchantmentGlint,
         BackpackProperties::new
     );
 
     public BackpackProperties setCosmetic(ResourceLocation model)
     {
-        return new BackpackProperties(Optional.of(model), this.showWithElytra, this.showEffects, this.showEnchantmentGlint);
+        return new BackpackProperties(Optional.of(model), this.showWithElytra, this.showEffects);
     }
 
     public BackpackProperties setShowWithElytra(boolean showWithElytra)
     {
-        return new BackpackProperties(this.cosmetic, showWithElytra, this.showEffects, this.showEnchantmentGlint);
+        return new BackpackProperties(this.cosmetic, showWithElytra, this.showEffects);
     }
 
     public BackpackProperties setShowEffects(boolean showEffects)
     {
-        return new BackpackProperties(this.cosmetic, this.showWithElytra, showEffects, this.showEnchantmentGlint);
-    }
-
-    public BackpackProperties setShowEnchantmentGlint(boolean showEnchantmentGlint)
-    {
-        return new BackpackProperties(this.cosmetic, this.showWithElytra, this.showEffects, showEnchantmentGlint);
+        return new BackpackProperties(this.cosmetic, this.showWithElytra, showEffects);
     }
 }
