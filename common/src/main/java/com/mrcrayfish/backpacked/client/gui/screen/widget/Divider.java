b096fcdf89adb4b6392b1b524205b299b8127f5a
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-09-27 22:13:28 +0930

    📦 Add divider widget

---
+++ common/src/main/java/com/mrcrayfish/backpacked/client/gui/screen/widget/Divider.java
@@ -0,0 +1,46 @@
+package com.mrcrayfish.backpacked.client.gui.screen.widget;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.network.chat.CommonComponents;
+
+public class Divider extends AbstractWidget
+{
+    private int colour = 0xFFFFFFFF;
+
+    private Divider(Orientation orientation, int size)
+    {
+        super(0, 0, orientation == Orientation.HORIZONTAL ? size : 1, orientation == Orientation.VERTICAL ? size : 1, CommonComponents.EMPTY);
+    }
+
+    public Divider colour(int colour)
+    {
+        this.colour = colour;
+        return this;
+    }
+
+    @Override
+    protected void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float partialTick)
+    {
+        graphics.fill(this.getX(), this.getY(), this.getX() + this.getWidth(), this.getY() + this.getHeight(), this.colour);
+    }
+
+    @Override
+    protected void updateWidgetNarration(NarrationElementOutput narrationElementOutput) {}
+
+    public enum Orientation
+    {
+        VERTICAL, HORIZONTAL;
+    }
+
+    public static Divider horizontal(int size)
+    {
+        return new Divider(Orientation.HORIZONTAL, size);
+    }
+
+    public static Divider vertical(int size)
+    {
+        return new Divider(Orientation.VERTICAL, size);
+    }
+}
