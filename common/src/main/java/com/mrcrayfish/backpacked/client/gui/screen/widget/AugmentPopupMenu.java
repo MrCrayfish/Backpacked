a6ca24cff2ae258cb4bb2ed747e62451b04af275
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-09-28 17:17:42 +0930

    📦 Improve augment popup menu layout

--- common/src/main/java/com/mrcrayfish/backpacked/client/gui/screen/widget/AugmentPopupMenu.java
+++ common/src/main/java/com/mrcrayfish/backpacked/client/gui/screen/widget/AugmentPopupMenu.java
@@ -1,34 +1,36 @@
 package com.mrcrayfish.backpacked.client.gui.screen.widget;
 
-import com.mrcrayfish.backpacked.client.gui.screen.layout.BorderedGridLayout;
+import com.mrcrayfish.backpacked.client.gui.screen.layout.BorderedLinearLayout;
 import com.mrcrayfish.backpacked.client.gui.screen.widget.popup.Alignment;
 import com.mrcrayfish.backpacked.client.gui.screen.widget.popup.PopupMenu;
 import com.mrcrayfish.backpacked.client.gui.screen.widget.popup.PopupMenuHandler;
 import com.mrcrayfish.backpacked.common.augment.Augment;
 import com.mrcrayfish.backpacked.common.augment.AugmentType;
-import com.mrcrayfish.backpacked.core.ModRegistries;
 import com.mrcrayfish.backpacked.util.Utils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.layouts.Layout;
+import net.minecraft.client.gui.layouts.LayoutSettings;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.network.chat.Component;
 
-import java.util.Comparator;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
-import java.util.stream.StreamSupport;
 
 public class AugmentPopupMenu extends PopupMenu
 {
     private static final int MAX_COLUMNS = 5;
 
-    private final BorderedGridLayout layout = (BorderedGridLayout) new BorderedGridLayout()
-            .border(2).rowSpacing(2).columnSpacing(2);
+    private final BorderedLinearLayout layout = (BorderedLinearLayout) new BorderedLinearLayout(LinearLayout.Orientation.VERTICAL).border(6).spacing(2);
 
     public AugmentPopupMenu(PopupMenuHandler handler, Augment<?> selected, Consumer<Augment<?>> updater)
     {
         super(handler);
-        this.setAlignment(Alignment.BELOW_LEFT);
-        this.setBackground(Utils.rl("backpack/dropdown/background"));
+        this.setAlignment(Alignment.END_TOP);
+        this.setBackground(Utils.rl("backpack/label"));
+        GridLayout grid = new GridLayout().rowSpacing(2).columnSpacing(2);
         AtomicInteger count = new AtomicInteger();
         AugmentType.stream().sorted().forEach(type -> {
             CustomButton augmentBtn = CustomButton.builder()
@@ -41,8 +43,12 @@ public class AugmentPopupMenu extends PopupMenu
             if(type == selected.type())
                 augmentBtn.active = false;
             int index = count.getAndIncrement();
-            this.layout.addChild(augmentBtn, index / MAX_COLUMNS, index % MAX_COLUMNS);
+            grid.addChild(augmentBtn, index / MAX_COLUMNS, index % MAX_COLUMNS);
         });
+        grid.arrangeElements(); // Do this so the divider can match the width
+        TextWidget title = this.layout.addChild(new TextWidget(Component.literal("Augments"), Minecraft.getInstance().font).setColour(0xFF61503D), layoutSettings -> layoutSettings.paddingHorizontal(10));
+        this.layout.addChild(Divider.horizontal(Math.max(grid.getWidth(), 10 + title.getWidth() + 10)).colour(0xFFE0CDB7));
+        this.layout.addChild(grid, LayoutSettings::alignHorizontallyLeft);
     }
 
     @Override
