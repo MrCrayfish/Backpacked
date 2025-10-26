2a53d78c1f67d7b718fd7c103dd9bcf1afd8d4b6
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-10-26 15:19:57 +1030

    📦 Migrate lightweaver light level to option layout

--- common/src/main/java/com/mrcrayfish/backpacked/client/augment/widget/LightweaverMenu.java
+++ common/src/main/java/com/mrcrayfish/backpacked/client/augment/widget/LightweaverMenu.java
@@ -15,6 +15,7 @@ import java.util.function.Supplier;
 
 public class LightweaverMenu extends AugmentSettingsMenu
 {
+    private static final Component OPTIONS_LABEL = Component.translatable("backpacked.gui.options");
     private static final Component LIGHT_LEVEL_LABEL = Component.translatable("augment.backpacked.lightweaver.light_level");
 
     private static final int MIN_CONTENT_WIDTH = 120;
@@ -23,18 +24,17 @@ public class LightweaverMenu extends AugmentSettingsMenu
     {
         super(handler, menu -> {
             LinearLayout layout = LinearLayout.vertical().spacing(2);
-            TitleWidget title = layout.addChild(new TitleWidget(LIGHT_LEVEL_LABEL, Minecraft.getInstance().font));
+            TitleWidget title = layout.addChild(new TitleWidget(OPTIONS_LABEL, Minecraft.getInstance().font));
             Divider divider = layout.addChild(Divider.horizontal(Math.max(MIN_CONTENT_WIDTH, title.getWidth())).colour(0xFFE0CDB7));
             title.setWidth(divider.getWidth());
-            Stepper lightLevelStepper = Stepper.builder()
-                .setSize(divider.getWidth(), 16)
-                .setInitialValue(supplier.get().minimumLight()) 
+            layout.addChild(createOption(LIGHT_LEVEL_LABEL, Stepper.builder()
+                .setSize(50, 16)
+                .setInitialValue(supplier.get().minimumLight())
                 .setMin(0)
                 .setMax(15)
                 .setOnChange(newValue -> {
                     updater.accept(supplier.get().setMinimumLight(newValue));
-                }).build();
-            layout.addChild(lightLevelStepper);
+                }).build(), divider.getWidth()));
             return layout;
         });
     }
