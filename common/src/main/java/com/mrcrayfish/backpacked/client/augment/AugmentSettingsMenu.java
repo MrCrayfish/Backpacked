0b271c81134851d2f36d8cce0ac01361a32a153a
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-10-26 15:19:35 +1030

    📦 Add option layout helper

--- common/src/main/java/com/mrcrayfish/backpacked/client/augment/AugmentSettingsMenu.java
+++ common/src/main/java/com/mrcrayfish/backpacked/client/augment/AugmentSettingsMenu.java
@@ -1,10 +1,17 @@
 package com.mrcrayfish.backpacked.client.augment;
 
 import com.mrcrayfish.backpacked.client.gui.screen.layout.PaddedLinearLayout;
+import com.mrcrayfish.backpacked.client.gui.screen.widget.TextWidget;
 import com.mrcrayfish.backpacked.client.gui.screen.widget.popup.PopupMenu;
 import com.mrcrayfish.backpacked.client.gui.screen.widget.popup.PopupMenuHandler;
 import com.mrcrayfish.backpacked.util.Utils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.layouts.Layout;
+import net.minecraft.client.gui.layouts.LayoutSettings;
+import net.minecraft.client.gui.layouts.LinearLayout;
+import net.minecraft.client.gui.layouts.SpacerElement;
+import net.minecraft.network.chat.Component;
 
 import java.util.function.Function;
 
@@ -25,5 +32,16 @@ public class AugmentSettingsMenu extends PopupMenu
     protected Layout layout()
     {
         return this.layout;
+    }
+
+    protected static Layout createOption(Component label, AbstractWidget widget, int width)
+    {
+        LinearLayout option = LinearLayout.horizontal().spacing(5);
+        TextWidget text = new TextWidget(label, Minecraft.getInstance().font);
+        text.setWidth(width - widget.getWidth() - 5);
+        text.setColour(0xFF61503D);
+        option.addChild(text, LayoutSettings::alignVerticallyMiddle);
+        option.addChild(widget);
+        return option;
     }
 }
