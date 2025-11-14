9b3e240dedcfb68790bce65155318a83ad51c89f
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-11-14 14:13:39 +1030

    🐛 Fix insert filter mode not working correctly

--- common/src/main/java/com/mrcrayfish/backpacked/mixin/common/HopperBlockEntityMixin.java
+++ common/src/main/java/com/mrcrayfish/backpacked/mixin/common/HopperBlockEntityMixin.java
@@ -56,7 +56,7 @@ public class HopperBlockEntityMixin
 
             Augments augments = Augments.get(inventory.getBackpackStack());
             HopperBridgeAugment augment = augments.findEnabledAndCast(ModAugmentTypes.HOPPER_BRIDGE.get());
-            if(augment != null && (!augment.insert() || !augment.isFilteringItem(stack.getItem())))
+            if(augment != null)
             {
                 if(!augment.insert() || augment.filterMode().checkInsert() && !augment.isFilteringItem(stack.getItem()))
                 {
