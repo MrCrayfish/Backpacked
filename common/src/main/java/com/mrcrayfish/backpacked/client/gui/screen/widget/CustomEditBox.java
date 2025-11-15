5180b82a60026c6915e7276a5784c83c91a03d32
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-11-15 15:39:12 +1030

    📦 Rework custom edit box to use a layout

--- common/src/main/java/com/mrcrayfish/backpacked/client/gui/screen/widget/CustomEditBox.java
+++ common/src/main/java/com/mrcrayfish/backpacked/client/gui/screen/widget/CustomEditBox.java
@@ -1,13 +1,14 @@
 package com.mrcrayfish.backpacked.client.gui.screen.widget;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mrcrayfish.backpacked.util.ScreenUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractContainerWidget;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.ImageWidget;
 import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.resources.ResourceLocation;
@@ -22,13 +23,19 @@ public class CustomEditBox extends AbstractContainerWidget
     private static final int ICON_PADDING = 2;
     private static final int ICON_SIZE = 12;
 
+    private final LinearLayout layout = LinearLayout.horizontal();
+
+    private final @Nullable ImageWidget iconWidget;
+    private final @Nullable WidgetSprites background;
     private final Impl editBox;
     private @Nullable Supplier<Boolean> activeSupplier;
 
     private CustomEditBox(int width, int height, @Nullable ResourceLocation icon, @Nullable WidgetSprites background)
     {
         super(0, 0, width, height, CommonComponents.EMPTY);
-        this.editBox = new Impl(this, icon, background);
+        this.background = background;
+        this.iconWidget = icon != null ? this.layout.addChild(ImageWidget.sprite(ICON_SIZE, ICON_SIZE, icon), s -> s.padding(ICON_PADDING)) : null;
+        this.editBox = this.layout.addChild(new Impl());
         this.setSize(width, height);
     }
 
@@ -42,27 +49,22 @@ public class CustomEditBox extends AbstractContainerWidget
     public void setX(int x)
     {
         super.setX(x);
-        int editBoxX = x;
-        if(this.editBox.icon != null)
-            editBoxX += this.getIconOffset();
-        this.editBox.setX(editBoxX);
+        this.layout.setX(x);
     }
 
     @Override
     public void setY(int y)
     {
         super.setY(y);
-        this.editBox.setY(y);
+        this.layout.setY(y);
     }
 
     @Override
     public void setWidth(int width)
     {
         super.setWidth(width);
-        int editBoxWidth = width;
-        if(this.editBox.icon != null)
-            editBoxWidth -= this.getIconOffset();
-        this.editBox.setWidth(editBoxWidth);
+        this.editBox.setWidth(this.iconWidget != null ? width - ICON_PADDING * 2 - ICON_SIZE : width);
+        this.layout.arrangeElements();
     }
 
     @Override
@@ -70,6 +72,7 @@ public class CustomEditBox extends AbstractContainerWidget
     {
         super.setHeight(height);
         this.editBox.setHeight(height);
+        this.layout.arrangeElements();
     }
 
     @Override
@@ -84,9 +87,21 @@ public class CustomEditBox extends AbstractContainerWidget
     {
         if(this.activeSupplier != null)
         {
-            this.editBox.active = this.activeSupplier.get();
+            boolean active = this.activeSupplier.get();
+            this.active = active;
+            this.editBox.active = active;
         }
-        this.editBox.render(graphics, mouseX, mouseY, partialTick);
+        if(this.background != null)
+        {
+            RenderSystem.enableBlend();
+            RenderSystem.enableDepthTest();
+            graphics.setColor(1, 1, 1, this.editBox.isActive() ? 1.0F : 0.5F);
+            ResourceLocation background = this.background.get(this.editBox.isActive(), this.editBox.isFocused());
+            graphics.blitSprite(background, this.getX(), this.getY(), this.getWidth(), this.getHeight());
+            graphics.setColor(1, 1, 1, 1);
+            RenderSystem.disableBlend();
+        }
+        this.layout.visitWidgets(widget -> widget.render(graphics,  mouseX, mouseY, partialTick));
     }
 
     @Override
@@ -113,11 +128,6 @@ public class CustomEditBox extends AbstractContainerWidget
         return this.editBox;
     }
 
-    private int getIconOffset()
-    {
-        return ICON_PADDING + ICON_SIZE + ICON_PADDING - 3;
-    }
-
     // TODO builder
     public static CustomEditBox create(int width, int height)
     {
@@ -132,47 +142,15 @@ public class CustomEditBox extends AbstractContainerWidget
 
     public static class Impl extends EditBox
     {
-        private final CustomEditBox parent;
-        private final @Nullable ResourceLocation icon;
-        private final @Nullable WidgetSprites background;
-
-        private Impl(CustomEditBox parent, @Nullable ResourceLocation icon, @Nullable WidgetSprites background)
+        private Impl()
         {
             super(Minecraft.getInstance().font, 0, 0, CommonComponents.EMPTY);
-            this.parent = parent;
-            this.icon = icon;
-            this.background = background;
         }
 
         @Override
         public boolean isBordered()
         {
-            return super.isBordered() && this.background == null;
-        }
-
-        @Override
-        public void renderWidget(GuiGraphics graphics, int mouseX, int mouseY, float partialTick)
-        {
-            if(this.isVisible())
-            {
-                if(this.background != null)
-                {
-                    RenderSystem.enableBlend();
-                    RenderSystem.enableDepthTest();
-                    graphics.setColor(1, 1, 1, this.isActive() ? 1.0F : 0.5F);
-                    ResourceLocation background = this.background.get(this.isActive(), this.isFocused());
-                    int iconOffset = this.icon != null ? this.parent.getIconOffset() : 0;
-                    graphics.blitSprite(background, this.getX() - iconOffset, this.getY(), this.getWidth() + iconOffset, this.getHeight());
-                    graphics.setColor(1, 1, 1, 1);
-                    RenderSystem.disableBlend();
-                }
-                if(this.icon != null)
-                {
-                    int iconY = this.getY() + (this.getHeight() - ICON_SIZE) / 2;
-                    graphics.blitSprite(this.icon, this.getX() - this.parent.getIconOffset() + ICON_PADDING, iconY, ICON_SIZE, ICON_SIZE);
-                }
-                super.renderWidget(graphics, mouseX, mouseY, partialTick);
-            }
+            return false; // Hack to disable the default background
         }
 
         @Override
@@ -185,22 +163,6 @@ public class CustomEditBox extends AbstractContainerWidget
                 return true;
             }
             return super.mouseClicked(mouseX, mouseY, button);
-        }
-
-        @Override
-        protected boolean clicked(double mouseX, double mouseY)
-        {
-            if(!this.active || !this.visible)
-                return false;
-            int editBoxX = this.getX();
-            int editBoxWidth = this.getWidth();
-            // Allow the area of the icon to be considered valid
-            if(this.icon != null)
-            {
-                editBoxX -= this.parent.getIconOffset();
-                editBoxWidth += this.parent.getIconOffset();
-            }
-            return ScreenUtil.isPointInArea((int) mouseX, (int) mouseY, editBoxX, this.getY(), editBoxWidth, this.getHeight());
         }
     }
 }
