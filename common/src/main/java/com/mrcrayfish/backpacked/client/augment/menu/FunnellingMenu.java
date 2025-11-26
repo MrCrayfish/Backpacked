497d97ed2af53dae17fdac5c797d55481d5bc41e
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2025-11-26 15:37:09 +1030

    📦 Switch funnelling to use ItemCollection/ItemGrid

--- common/src/main/java/com/mrcrayfish/backpacked/client/augment/menu/FunnellingMenu.java
+++ common/src/main/java/com/mrcrayfish/backpacked/client/augment/menu/FunnellingMenu.java
@@ -4,33 +4,23 @@ import com.mrcrayfish.backpacked.Config;
 import com.mrcrayfish.backpacked.Constants;
 import com.mrcrayfish.backpacked.client.augment.AugmentHolder;
 import com.mrcrayfish.backpacked.client.augment.AugmentSettingsMenu;
-import com.mrcrayfish.backpacked.client.gui.StateSprites;
 import com.mrcrayfish.backpacked.client.gui.screen.widget.*;
 import com.mrcrayfish.backpacked.client.gui.screen.widget.popup.PopupMenuHandler;
 import com.mrcrayfish.backpacked.common.augment.impl.FunnellingAugment;
 import com.mrcrayfish.backpacked.util.ScreenUtil;
 import com.mrcrayfish.backpacked.util.Utils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.minecraft.client.gui.components.Tooltip;
 import net.minecraft.client.gui.components.WidgetSprites;
 import net.minecraft.client.gui.layouts.LayoutSettings;
 import net.minecraft.client.gui.layouts.LinearLayout;
-import net.minecraft.client.resources.sounds.SimpleSoundInstance;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import org.jetbrains.annotations.Nullable;
-import org.lwjgl.glfw.GLFW;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 
-import java.util.Comparator;
-import java.util.Locale;
+import java.util.function.Predicate;
 
 public class FunnellingMenu extends AugmentSettingsMenu
 {
@@ -40,17 +30,26 @@ public class FunnellingMenu extends AugmentSettingsMenu
     private static final Component SHOW_ALL_LABEL = Component.translatable("backpacked.gui.show_all");
     private static final Component ACTIVATED_ONLY_LABEL = Component.translatable("backpacked.gui.activated_only");
 
-    private static final int MIN_CONTENT_WIDTH = 170;
+    private static final Predicate<Item> REMOVE_BLOCKS_WITHOUT_LOOT_TABLE = item -> {
+        // This removes most creative/operator blocks
+        if(item instanceof BlockItem blockItem) {
+            var key = blockItem.getBlock().getLootTable();
+            return key != BuiltInLootTables.EMPTY;
+        }
+        return true;
+    };
+
+    private static final int MIN_CONTENT_WIDTH = 162;
 
     private static String lastQuery = "";
-    private static boolean lastFilter = false;
+    private static boolean selectedOnly = false;
 
     public FunnellingMenu(PopupMenuHandler handler, AugmentHolder<FunnellingAugment> holder)
     {
         super(handler, menu -> {
             LinearLayout layout = LinearLayout.vertical().spacing(2);
             TitleWidget title = layout.addChild(new TitleWidget(() -> {
-                int filterCount = holder.get().filters().size();
+                int filterCount = holder.get().filters().ids().size();
                 int maxFilters = Config.AUGMENTS.funnelling.maxFilters.get();
                 Component amount = Component.translatable("backpacked.gui.x_of_y", filterCount, maxFilters);
                 return ScreenUtil.join(" ", FILTERS_LABEL, amount);
@@ -58,7 +57,13 @@ public class FunnellingMenu extends AugmentSettingsMenu
             Divider divider = layout.addChild(Divider.horizontal(Math.max(MIN_CONTENT_WIDTH, title.getWidth())).colour(0xFFE0CDB7));
             title.setWidth(divider.getWidth());
 
-            FilterList list = new FilterList(holder, divider.getWidth(), lastQuery, lastFilter);
+            ItemGrid<FunnellingAugment> grid = ItemGrid.builder(holder::get, holder::update)
+                .setWidth(divider.getWidth())
+                .setHeight(84)
+                .setInitialQuery(lastQuery)
+                .setPredicate(REMOVE_BLOCKS_WITHOUT_LOOT_TABLE)
+                .build();
+            grid.setSelectedOnly(selectedOnly);
 
             int filterButtonWidth = 55;
             LinearLayout header = LinearLayout.horizontal().spacing(3);
@@ -68,190 +73,34 @@ public class FunnellingMenu extends AugmentSettingsMenu
                 .setIcon(Utils.rl("backpack/editbox/search"))
                 .setText(lastQuery)
                 .setHint(SEARCH_HINT)
-                .setCallback(list::setSearchQuery)
+                .setCallback(s -> {
+                    grid.setSearchQuery(s);
+                    lastQuery = s;
+                })
                 .setBackground(new WidgetSprites(
                     Utils.rl("backpack/editbox/background"),
                     Utils.rl("backpack/editbox/background_focused")
                 )).build();
             header.addChild(searchField, LayoutSettings::alignVerticallyMiddle);
-            header.addChild(CustomButton.state(list::isActivatedOnly, list::setActivatedOnly)
+            header.addChild(CustomButton.state(() -> selectedOnly, newValue -> selectedOnly = newValue)
                 .setSize(filterButtonWidth, 18)
                 .setGap(4)
                 .setMessage(ACTIVE_LABEL)
                 .setContentRenderer(CustomButton.ToggleContentRenderer.INSTANCE)
-                .setTooltip(btn -> Tooltip.create(list.isActivatedOnly() ? ACTIVATED_ONLY_LABEL : SHOW_ALL_LABEL))
+                .setTooltip(btn -> Tooltip.create(selectedOnly ? ACTIVATED_ONLY_LABEL : SHOW_ALL_LABEL))
+                .setAction(btn -> grid.setSelectedOnly(selectedOnly))
                 .setTexture(new WidgetSprites(
                     ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/button_enabled"),
                     ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/button_enabled_focused")
                 )).build());
             layout.addChild(header);
 
-            layout.addChild(list);
+            layout.addChild(grid);
             layout.addChild(CustomButton.values(() -> holder.get().mode(), mode -> holder.update(holder.get().setMode(mode)))
                 .setSize(divider.getWidth(), 18)
                 .build()
             );
             return layout;
         });
-    }
-
-    private static final class FilterList extends CustomSelectionList<FilterList.FilterItem>
-    {
-        private static final ResourceLocation LIST_BACKGROUND_SPRITE = ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/list/background");
-        private static final StateSprites ITEM_SPRITES = new StateSprites(
-            ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/list/item"),
-            ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/list/item_hovered"),
-            ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/list/item_selected")
-        );
-        private static final StateSprites SCROLL_BAR_SPRITES = new StateSprites(
-            ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/list/scroll_bar"),
-            ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/list/scroll_bar_hovered"),
-            ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack/list/scroll_bar_selected")
-        );
-
-        private final AugmentHolder<FunnellingAugment> holder;
-        private String searchQuery = "";
-        private boolean activatedOnly = false;
-
-        public FilterList(AugmentHolder<FunnellingAugment> holder, int width, String lastQuery, boolean lastFilter)
-        {
-            super(width, 104, 0, 0, 18);
-            this.holder = holder;
-            this.setRenderHeader(false, 0);
-            this.setListBackground(LIST_BACKGROUND_SPRITE);
-            this.setItemSprites(ITEM_SPRITES);
-            this.setScrollBarSprites(SCROLL_BAR_SPRITES);
-            this.setContentPadding(2);
-            this.setItemSpacing(2);
-            this.setScrollBarWidth(10);
-            this.setScrollBarStyle(ScrollBarStyle.DETACHED);
-            this.searchQuery = lastQuery;
-            this.activatedOnly = lastFilter;
-            this.updateList();
-        }
-
-        private void updateList()
-        {
-            String search = this.searchQuery.toLowerCase();
-            boolean empty = search.trim().isBlank();
-            this.clearEntries();
-            BuiltInRegistries.ITEM.forEach(item -> {
-                if(item == Items.AIR)
-                    return;
-                if(empty || item.getDescription().getString().toLowerCase(Locale.ROOT).contains(search)) {
-                    if(!this.activatedOnly || this.holder.get().isFilter(item)) {
-                        this.addEntry(new FilterItem(item, this.holder));
-                    }
-                }
-            });
-            this.children().sort(Comparator.comparing(item -> item.label.getString()));
-        }
-
-        @Override
-        public void setSelected(@Nullable FunnellingMenu.FilterList.FilterItem item)
-        {
-            if(item != null)
-            {
-                FunnellingAugment augment = this.holder.get();
-                if(!item.isToggled())
-                {
-                    if(!augment.isFilterLimit())
-                    {
-                        this.holder.update(augment.addFilter(item.id));
-                        Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
-                    }
-                }
-                else
-                {
-                    this.holder.update(augment.removeFilter(item.id));
-                    Minecraft.getInstance().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
-                }
-            }
-        }
-
-        private void setSearchQuery(String searchQuery)
-        {
-            lastQuery = searchQuery;
-            this.searchQuery = searchQuery;
-            this.updateList();
-        }
-
-        private void setActivatedOnly(boolean activatedOnly)
-        {
-            lastFilter = activatedOnly;
-            this.activatedOnly = activatedOnly;
-            this.updateList();
-            this.setScrollAmount(0);
-        }
-
-        public boolean isActivatedOnly()
-        {
-            return this.activatedOnly;
-        }
-
-        public final class FilterItem extends ObjectSelectionList.Entry<FilterItem>
-        {
-            private static final ResourceLocation TOGGLE_OFF = Utils.rl("backpack/toggle_off");
-            private static final ResourceLocation TOGGLE_ON = Utils.rl("backpack/toggle_on");
-
-            private final ResourceLocation id;
-            private final ItemStack display;
-            private final Component label;
-            private final AugmentHolder<FunnellingAugment> holder;
-
-            public FilterItem(Item item, AugmentHolder<FunnellingAugment> holder)
-            {
-                this.id = BuiltInRegistries.ITEM.getKey(item);
-                this.display = new ItemStack(item);
-                this.label = this.trimName(this.display.getItem().getDescription());
-                this.holder = holder;
-            }
-
-            private Component trimName(Component name)
-            {
-                Font font = Minecraft.getInstance().font;
-                String rawName = name.getString();
-                int maxWidth = FilterList.this.getRowWidth() - 20 - 15;
-                if(font.width(rawName) > maxWidth)
-                {
-                    rawName = font.plainSubstrByWidth(rawName, maxWidth - font.width("...")).trim() + "...";
-                }
-                return Component.literal(rawName);
-            }
-
-            @Override
-            public void render(GuiGraphics graphics, int index, int top, int left, int rowWidth, int rowHeight, int mouseX, int mouseY, boolean hovered, float partialTicks)
-            {
-                boolean showToggle = this.isToggled() || !this.holder.get().isFilterLimit();
-                int labelColour = showToggle ? 0xFFFFFFFF : 0x88FFFFFF;
-                graphics.drawString(Minecraft.getInstance().font, this.label, left + 20, top + 5, labelColour);
-                graphics.renderFakeItem(this.display, left + 2, top + 1);
-                if(showToggle)
-                {
-                    graphics.blitSprite(this.isToggled() ? TOGGLE_ON : TOGGLE_OFF, left + rowWidth - 6 - 6, top + 6, 6, 6);
-                }
-            }
-
-            @Override
-            public Component getNarration()
-            {
-                return this.display.getDisplayName();
-            }
-
-            @Override
-            public boolean mouseClicked(double mouseX, double mouseY, int button)
-            {
-                if(button == GLFW.GLFW_MOUSE_BUTTON_LEFT)
-                {
-                    return this.isToggled() || !this.holder.get().isFilterLimit();
-                }
-                return false;
-            }
-
-            public boolean isToggled()
-            {
-                return this.holder.get().isFilter(this.display.getItem());
-            }
-        }
     }
 }
--- common/src/main/java/com/mrcrayfish/backpacked/common/augment/impl/FunnellingAugment.java
+++ common/src/main/java/com/mrcrayfish/backpacked/common/augment/impl/FunnellingAugment.java
@@ -1,10 +1,11 @@
 package com.mrcrayfish.backpacked.common.augment.impl;
 
-import com.google.common.collect.ImmutableMap;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.mrcrayfish.backpacked.Config;
 import com.mrcrayfish.backpacked.client.LabelAndDescription;
+import com.mrcrayfish.backpacked.common.FilterableItems;
+import com.mrcrayfish.backpacked.common.ItemCollection;
 import com.mrcrayfish.backpacked.common.augment.Augment;
 import com.mrcrayfish.backpacked.common.augment.AugmentType;
 import com.mrcrayfish.backpacked.util.Utils;
@@ -18,39 +19,30 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-public final class FunnellingAugment implements Augment<FunnellingAugment>
+public record FunnellingAugment(ItemCollection filters, Mode mode) implements Augment<FunnellingAugment>, FilterableItems<FunnellingAugment>
 {
     public static final AugmentType<FunnellingAugment> TYPE = new AugmentType<>(
-            Utils.rl("funnelling"),
-            RecordCodecBuilder.mapCodec(instance -> instance.group(
-                ItemFilter.CODEC.sizeLimitedListOf(256).fieldOf("filters").orElse(List.of()).forGetter(FunnellingAugment::filters),
-                Mode.CODEC.fieldOf("mode").orElse(Mode.ALLOW).forGetter(FunnellingAugment::mode)
-            ).apply(instance, FunnellingAugment::new)),
-            StreamCodec.composite(
-                ItemFilter.STREAM_CODEC.apply(ByteBufCodecs.list()),
-                FunnellingAugment::filters,
-                Mode.STREAM_CODEC,
-                FunnellingAugment::mode,
-                FunnellingAugment::new
-            ),
-            () -> new FunnellingAugment(List.of(), Mode.ALLOW)
+        Utils.rl("funnelling"),
+        RecordCodecBuilder.mapCodec(instance -> instance.group(
+            ItemCollection.CODEC.fieldOf("filters").orElse(ItemCollection.EMPTY).forGetter(FunnellingAugment::filters),
+            Mode.CODEC.fieldOf("mode").orElse(Mode.ALLOW).forGetter(FunnellingAugment::mode)
+        ).apply(instance, FunnellingAugment::new)),
+        StreamCodec.composite(
+            ItemCollection.STREAM_CODEC,
+            FunnellingAugment::filters,
+            Mode.STREAM_CODEC,
+            FunnellingAugment::mode,
+            FunnellingAugment::new
+        ),
+        () -> new FunnellingAugment(ItemCollection.EMPTY, Mode.ALLOW)
     );
-    private static final ResourceLocation AIR = ResourceLocation.withDefaultNamespace("air");
-    private final List<ItemFilter> filters;
-    private final Mode mode;
-    private @Nullable Map<Item, List<ItemFilter>> lookup;
 
-    public FunnellingAugment(List<ItemFilter> filters, Mode mode)
+    public FunnellingAugment
     {
-        this.filters = filters.stream().filter(filter -> {
-            return BuiltInRegistries.ITEM.containsKey(filter.id) && !filter.id.equals(AIR);
-        }).limit(Config.AUGMENTS.funnelling.maxFilters.get()).collect(Collectors.toList());
-        this.mode = mode;
+        filters = filters.limit(Config.AUGMENTS.funnelling.maxFilters.get());
     }
 
     @Override
@@ -59,24 +51,28 @@ public final class FunnellingAugment implements Augment<FunnellingAugment>
         return TYPE;
     }
 
-    public FunnellingAugment addFilter(ResourceLocation id)
+    @Override
+    public FunnellingAugment addItemFilter(Item item)
     {
-        List<ItemFilter> filters = new ArrayList<>(this.filters);
-        filters.add(new ItemFilter(id));
-        return new FunnellingAugment(filters, this.mode);
+        return new FunnellingAugment(this.filters.add(item), this.mode);
     }
 
-    public FunnellingAugment removeFilter(ResourceLocation id)
+    @Override
+    public FunnellingAugment removeItemFilter(Item item)
     {
-        List<ItemFilter> filters = new ArrayList<>(this.filters);
-        filters.removeIf(filter -> filter.id.equals(id));
-        return new FunnellingAugment(filters, this.mode);
+        return new FunnellingAugment(this.filters.remove(item), this.mode);
     }
 
-    public boolean isFilter(Item item)
+    @Override
+    public boolean isFilteringItem(Item item)
     {
-        Map<Item, List<ItemFilter>> lookup = this.buildLookup();
-        return lookup.containsKey(item);
+        return this.filters.has(item);
+    }
+
+    @Override
+    public boolean isFilterFull()
+    {
+        return this.filters.ids().size() >= Config.AUGMENTS.funnelling.maxFilters.get();
     }
 
     public FunnellingAugment setMode(Mode mode)
@@ -84,70 +80,14 @@ public final class FunnellingAugment implements Augment<FunnellingAugment>
         return new FunnellingAugment(this.filters, mode);
     }
 
-    public List<ItemFilter> filters()
-    {
-        return this.filters;
-    }
-
-    public Mode mode()
-    {
-        return this.mode;
-    }
-
     public boolean test(ItemStack stack)
     {
-        Map<Item, List<ItemFilter>> lookup = this.buildLookup();
-        boolean matched = this.testFilters(stack, lookup.getOrDefault(stack.getItem(), Collections.emptyList()));
+        boolean matched = this.filters.has(stack.getItem());
         if(matched && this.mode == Mode.ALLOW)
             return true;
         if(matched && this.mode == Mode.DISALLOW)
             return false;
         return this.mode == Mode.DISALLOW;
-    }
-
-    private boolean testFilters(ItemStack stack, List<ItemFilter> filters)
-    {
-        for(ItemFilter filter : filters)
-        {
-            if(filter.match(stack))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private Map<Item, List<ItemFilter>> buildLookup()
-    {
-        if(this.lookup == null)
-        {
-            Map<Item, List<ItemFilter>> map = new HashMap<>();
-            this.filters.forEach(filter -> map.computeIfAbsent(filter.item(), k -> new ArrayList<>()).add(filter));
-            ImmutableMap.Builder<Item, List<ItemFilter>> builder = ImmutableMap.builder();
-            map.forEach((item, filters) -> builder.put(item, Collections.unmodifiableList(filters)));
-            this.lookup = builder.build();
-        }
-        return this.lookup;
-    }
-
-    public boolean isFilterLimit()
-    {
-        return this.filters.size() >= Config.AUGMENTS.funnelling.maxFilters.get();
-    }
-
-    @Override
-    public boolean equals(Object obj)
-    {
-        if(obj == this) return true;
-        if(obj == null || obj.getClass() != this.getClass()) return false;
-        var that = (FunnellingAugment) obj;
-        return Objects.equals(this.filters, that.filters) && Objects.equals(this.mode, that.mode);
-    }
-
-    @Override
-    public int hashCode()
-    {
-        return Objects.hash(this.filters, this.mode);
     }
 
     public static final class ItemFilter
@@ -217,7 +157,7 @@ public final class FunnellingAugment implements Augment<FunnellingAugment>
 
         Mode()
         {
-            this.name = Component.translatable("%s.%s".formatted(LANGUAGE_KEY, this.getSerializedName()));
+            this.name = Component.translatable(LANGUAGE_KEY, Component.translatable("%s.%s".formatted(LANGUAGE_KEY, this.getSerializedName())));
             this.tooltip = Component.translatable("%s.%s.tooltip".formatted(LANGUAGE_KEY, this.getSerializedName()));
         }
 
@@ -229,7 +169,7 @@ public final class FunnellingAugment implements Augment<FunnellingAugment>
 
         public Component label()
         {
-            return Component.translatable(LANGUAGE_KEY, this.name);
+            return this.name;
         }
 
         public Component description()
