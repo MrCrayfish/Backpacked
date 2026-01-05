626863bd9c8ff5dfa00109399fa38805cc91bbc8
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-01-05 22:15:02 +1030

    📦 Add unlock token item

--- common/src/main/java/com/mrcrayfish/backpacked/core/ModItems.java
+++ common/src/main/java/com/mrcrayfish/backpacked/core/ModItems.java
@@ -1,10 +1,9 @@
 package com.mrcrayfish.backpacked.core;
 
-import com.mrcrayfish.backpacked.Constants;
 import com.mrcrayfish.backpacked.platform.Services;
+import com.mrcrayfish.backpacked.util.Utils;
 import com.mrcrayfish.framework.api.registry.RegistryContainer;
 import com.mrcrayfish.framework.api.registry.RegistryEntry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 
 /**
@@ -13,5 +12,6 @@ import net.minecraft.world.item.Item;
 @RegistryContainer
 public class ModItems
 {
-    public static final RegistryEntry<Item> BACKPACK = RegistryEntry.item(ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "backpack"), () -> Services.BACKPACK.createBackpackItem(new Item.Properties().stacksTo(1)));
+    public static final RegistryEntry<Item> BACKPACK = RegistryEntry.item(Utils.rl("backpack"), () -> Services.BACKPACK.createBackpackItem(new Item.Properties().stacksTo(1)));
+    public static final RegistryEntry<Item> UNLOCK_TOKEN = RegistryEntry.item(Utils.rl("unlock_token"), () -> new Item(new Item.Properties()));
 }
