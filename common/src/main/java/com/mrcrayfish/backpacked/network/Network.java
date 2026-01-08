3d94495bc45c59ed17b3db2a64c95ae375503e32
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-01-08 21:38:17 +1030

    📦 Update network to registry contain

--- common/src/main/java/com/mrcrayfish/backpacked/network/Network.java
+++ common/src/main/java/com/mrcrayfish/backpacked/network/Network.java
@@ -5,6 +5,7 @@ import com.mrcrayfish.backpacked.common.backpack.BackpackManager;
 import com.mrcrayfish.backpacked.network.message.*;
 import com.mrcrayfish.framework.api.FrameworkAPI;
 import com.mrcrayfish.framework.api.network.FrameworkNetwork;
+import com.mrcrayfish.framework.api.registry.RegistryContainer;
 import net.minecraft.network.protocol.PacketFlow;
 import net.minecraft.resources.Identifier;
 
@@ -13,6 +14,7 @@ import java.util.List;
 /**
  * Author: MrCrayfish
  */
+@RegistryContainer
 public class Network
 {
     public static final FrameworkNetwork PLAY = FrameworkAPI
