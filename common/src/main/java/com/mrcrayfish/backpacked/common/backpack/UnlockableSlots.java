64dfb488f04df755995d2554dc9d2079288cd399
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-02-04 12:42:36 +1030

    📦 Remove assert

--- common/src/main/java/com/mrcrayfish/backpacked/common/backpack/UnlockableSlots.java
+++ common/src/main/java/com/mrcrayfish/backpacked/common/backpack/UnlockableSlots.java
@@ -68,7 +68,6 @@ public final class UnlockableSlots
 
     private UnlockableSlots(Set<Integer> slots, int maxSlots)
     {
-        assert maxSlots > 0;
         this.slots = slots;
         this.maxSlots = maxSlots;
         this.nextCount = calculateUnlockedCount(slots, maxSlots);
