deea16a0e43a7608ae16a5bb89f276ab3a277291
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-02-13 12:21:01 +1030

    🐛 Potential fix for forge/bukkit hybrid servers
    Close #281

--- forge/src/main/java/com/mrcrayfish/backpacked/mixin/common/ForgeLivingEntityMixin.java
+++ forge/src/main/java/com/mrcrayfish/backpacked/mixin/common/ForgeLivingEntityMixin.java
@@ -4,7 +4,6 @@ import com.mrcrayfish.backpacked.Config;
 import com.mrcrayfish.backpacked.common.augment.AugmentHandler;
 import com.mrcrayfish.backpacked.core.ModSyncedDataKeys;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -12,10 +11,8 @@ import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(LivingEntity.class)
 public class ForgeLivingEntityMixin
@@ -23,10 +20,12 @@ public class ForgeLivingEntityMixin
     @Unique
     private InteractionHand backpacked$hand;
 
-    @Inject(method = "checkTotemDeathProtection", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void backpacked$CheckBackpackForTotem(DamageSource p_21263_, CallbackInfoReturnable<Boolean> cir, ItemStack itemstack, InteractionHand[] var3, int var4, int var5, InteractionHand hand)
+    // Capturing like this to satisfy both Forge and Bukkit Hybrid servers
+    @ModifyArg(method = "checkTotemDeathProtection", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;"), index = 0)
+    private InteractionHand backpacked$CheckBackpackForTotem_Normal(InteractionHand hand)
     {
         this.backpacked$hand = hand;
+        return hand;
     }
 
     @ModifyVariable(method = "checkTotemDeathProtection", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/entity/LivingEntity;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;"), index = 7)
