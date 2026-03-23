fc7040731b52fa06792c7e22a20d4d0ba3baff50
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2026-03-23 16:59:01 +1030

    📦 Add backpack dock as mineable with axe

--- common/src/main/java/com/mrcrayfish/backpacked/datagen/CommonBlockTagGen.java
+++ common/src/main/java/com/mrcrayfish/backpacked/datagen/CommonBlockTagGen.java
@@ -30,7 +30,8 @@ public class CommonBlockTagGen extends IntrinsicHolderTagsProvider<Block>
             ModBlocks.CRIMSON_BACKPACK_SHELF.get(),
             ModBlocks.WARPED_BACKPACK_SHELF.get(),
             ModBlocks.CHERRY_BACKPACK_SHELF.get(),
-            ModBlocks.PALE_OAK_BACKPACK_SHELF.get()
+            ModBlocks.PALE_OAK_BACKPACK_SHELF.get(),
+            ModBlocks.BACKPACK_DOCK.get()
         );
     }
 }
